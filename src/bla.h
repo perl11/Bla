2952826a18a1c383ccc0fdf1c660683ae8d28051
Reini Urban <rurban@cpanel.net>
2014-11-29 19:50:58 +0100

    README: bsd license, fix links. MEM_ALIGN portability

--- src/bla.h
+++ src/bla.h
@@ -3,5 +3,5 @@
 #define NIL 0
 #define TRUE 1
 #define FALSE 0
-#define MEM_ALIGN 7
-// alignment for memory-allocs: 3 for 32bit, 7 for 64bit etc.
+// alignment for memory-allocs: 3 for 32bit, 7 for 64bit
+#define MEM_ALIGN (sizeof(void*)-1)
